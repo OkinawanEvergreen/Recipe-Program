d7b35c6fbcd3e796bdf30bc9c179d4d63b7ee8f5
OkinawanEvergreen <81052814+OkinawanEvergreen@users.noreply.github.com>
2022-07-05 20:20:25 -0500

    Update SteppingStone3_Branches.java

--- SteppingStone3_Branches.java
+++ SteppingStone3_Branches.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package SteppingStones;
 
 import java.util.Scanner;
@@ -53,7 +48,7 @@ public class SteppingStone3_Branches {
                  
            }
        } else {
-           System.out.println("Error: That is not a numer. Try again.");
+           System.out.println("Error: That is not a number. Try again.");
        }
     }
 }
